c29acf73df79319eebbeb3dded92bb1b249ecc9d
Ponca Labor01 <polbr01@safeway.com>
2023-08-28 20:28:08 -0500

    Deploy webhook att2

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,29 +1,32 @@
-import AccountProfile from "@/components/forms/AccountProfile"
-import { currentUser } from "@clerk/nextjs"
+import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 const Page = async () => {
-  const user = await currentUser();
-  const userInfo={}
-  const userData = {
-    id: user?.id,
-    objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.info || "",
-    image: userInfo?.image || user?.imageUrl
-  }
-  
-  return (
-    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-        <h1 className="head-text">OnBoarding</h1>
-        <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Threads</p>
-        <section className="mt-9 bg-dark-2 p-10">
-          <AccountProfile 
-          user={userData}
-          btnTitle="Continue"
-          />
-        </section>
-    </main>
-  )
-}
+	const user = await currentUser();
+	if (!user) return null;
+	const userInfo = await fetchUser(user.id);
+	if (userInfo?.onboarded) redirect("/");
+	const userData = {
+		id: user?.id,
+		objectId: userInfo?._id,
+		username: userInfo?.username || user?.username,
+		name: userInfo?.name || user?.firstName || "",
+		bio: userInfo?.info || "",
+		image: userInfo?.image || user?.imageUrl,
+	};
 
-export default Page
+	return (
+		<main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+			<h1 className="head-text">OnBoarding</h1>
+			<p className="mt-3 text-base-regular text-light-2">
+				Complete your profile now to use Threads
+			</p>
+			<section className="mt-9 bg-dark-2 p-10">
+				<AccountProfile user={userData} btnTitle="Continue" />
+			</section>
+		</main>
+	);
+};
+
+export default Page;
